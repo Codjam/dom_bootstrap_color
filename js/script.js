a5dd0494b18506a4ef8021e99c12c86671f91513
Codjam <jamaltbutler>
2019-02-02 00:47:38 -0500

    added more colors to the array list

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //create an array
-const colors = ['yellow', 'green', 'blue', '#73FBD3', 'rgba(125,125,125, 0.5']
+const colors = ['yellow', 'green', 'blue', '#73FBD3', 'rgba(125,125,125, 0.5', 'purple', 'orange', '#73FBD3', '#D81159', '#F3C98B', '#D1BCE3', '#C6D8AF']
 
 const btn = document.getElementById('btn');
 
